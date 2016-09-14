5c36493a97e417ae3e505b9e2d38476a337f6fa2
Ernesto Valero <evalero@paradigmadigital.com>
2016-09-14 16:30:38 +0200

    se modifica el fichero para la carga de datos se añade el dump para el ejercicio de resinc

--- Tema1/ReplicaSet/populateDB.js
+++ Tema1/ReplicaSet/populateDB.js
@@ -1,6 +1,9 @@
 db=db.getSiblingDB("miBD");
 
+print ("introduciendo datos en la BBDD miBD")
+
 for (i=0 ; i < 10000 ; i ++) {
    db.muchosDatos.insert({x:i,y:i+2,z:i*2})
 }
-
+print("Configuración original del replica set :");
+printjson(rs.config());
